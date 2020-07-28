cdaeeb50d0cfa9add8bc56fd78e8aa404f840979
Preetish Niket <niketpreetish@gmail.com>
2020-07-28 16:42:30 +0530

    ChangedDirectory

---
+++ app/src/main/java/com/example/note/AddTaskActivity.kt
@@ -0,0 +1,56 @@
+package com.example.note
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add_task.*
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+        number_picker_priority.maxValue=10
+        number_picker_priority.minValue=1
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        supportActionBar?.elevation=4.4f
+        supportActionBar?.title="Add Task"
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.add_task_menu,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.save_note -> {
+                saveNotes()
+                true
+            }
+            else ->
+                super.onOptionsItemSelected(item)
+        }
+    }
+    companion object{
+        const val EXTRA_TITLE="title"
+        const val EXTRA_DESCRIPTION="description"
+        const val EXTRA_PRIORITY="priority"
+    }
+    private fun saveNotes() {
+        val title: String = edit_text_title.text.toString()
+        val description: String = edit_text_description.text.toString()
+        val priority: Int = number_picker_priority.value
+        val data = Intent()
+        if (title.trim { it <= ' ' }.isEmpty() || description.trim { it <= ' ' }.isEmpty()) {
+            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_SHORT).show()
+        }
+        data.putExtra(EXTRA_TITLE, title)
+        data.putExtra(EXTRA_DESCRIPTION, description)
+        data.putExtra(EXTRA_PRIORITY, priority)
+        setResult(Activity.RESULT_OK, data)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.note
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+       fab.setOnClickListener {
+            startActivity(Intent(this,AddTaskActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
+//override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//    super.onViewCreated(view, savedInstanceState)
+//
+//    view.findViewById<Button>(R.id.button_first).setOnClickListener {
+//        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//    }
+//}
